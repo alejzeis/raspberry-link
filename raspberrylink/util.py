a683a46c1f34af6dc42b2deef3adb6b75b8015e5
jython234 <jython234@users.noreply.github.com>
2020-03-15 22:26:42 -0500

    Use subprocess instead

--- raspberrylink/util.py
+++ raspberrylink/util.py
@@ -1,6 +1,6 @@
-from os import system
+from subprocess import run
 
 def check_audio_support():
     # Check to see if the raspberrylink-audio service has ran (or if it even exists)
     # A != 0 exit code implies something is wrong or the service doesn't exist, thus no bluetooth audio support for us
-    return system("systemctl status raspberrylink-audio") == 0
+    return run("systemctl status raspberrylink-audio").returncode == 0
