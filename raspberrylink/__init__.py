d3c107a7f432c7fd97b09be0b918487cdc5a9686
jython234 <jython234@users.noreply.github.com>
2020-03-15 21:08:24 -0500

    Add systemd service files, begin python packaging

--- raspberrylink/__init__.py
+++ raspberrylink/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 software_name = "Raspberry-link"
-software_version = "1.0.0"
+software_version = "1.0.0-pre"
 api_version = 1
 
 from raspberrylink import obd
@@ -10,3 +10,9 @@ obdmanager = obd.OBDManager()
 app = Flask(__name__)
 
 from raspberrylink import routes
+
+
+def run_server():
+    from waitress import serve
+    # app.run()
+    serve(app)
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from setuptools import setup
+__author__ = "jython234"
+
+setup(
+  name='raspberrylink-server',
+  version='1.0.0-pre',
+  description='Raspberrylink WSGI Information Server',
+  author=__author__,
+  author_email='jython234@gmail.com',
+  license='MPL-2.0',
+  url='https://github.com/jython234/raspberrylink',
+  packages=['raspberrylink'],
+  install_requires=['flask', 'obd', 'waitress'],
+  entry_points={
+    'console_scripts': [
+      'raspilink-server=raspberrylink:run_server',
+    ]
+  },
+)
