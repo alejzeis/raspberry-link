69e9a70ffd7d30f067883b261a4aaae3c8350610
Alejandro Zeise <jython234@users.noreply.github.com>
2021-01-09 15:50:21 -0600

    Update build_dpkg.sh

--- build_dpkg.sh
+++ build_dpkg.sh
@@ -3,6 +3,7 @@
 rm -rf dist
 rm -rf deb_dist
 
-chmod +x bt-audio/raspilink-audio-start
+chmod +x bluetooth-scripts/raspilink-bt-agent.py
+chmod +x bluetooth-scripts/raspilink-bt-init
 
 DEB_BUILD_OPTIONS=nocheck python3 setup.py --command-packages=stdeb.command bdist_deb
